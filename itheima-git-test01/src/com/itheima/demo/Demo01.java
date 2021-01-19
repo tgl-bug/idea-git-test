87fb2b6ff208bac74c74eb03ad81eb6d7339f601
tgl-bug <tgl812664988@gmail.com>
2021-01-19 13:47:26 +0800

    Revert "第四次"
    This reverts commit 717c23b0

--- itheima-git-test01/src/com/itheima/demo/Demo01.java
+++ itheima-git-test01/src/com/itheima/demo/Demo01.java
@@ -3,5 +3,10 @@ package com.itheima.demo;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
