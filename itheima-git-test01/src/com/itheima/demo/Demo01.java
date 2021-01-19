80e46eae3c26420eecadc06b3b1e75baf1ca560e
tgl-bug <tgl812664988@gmail.com>
2021-01-19 13:35:47 +0800

    第二次使用idea提交代码

--- itheima-git-test01/src/com/itheima/demo/Demo01.java
+++ itheima-git-test01/src/com/itheima/demo/Demo01.java
@@ -3,5 +3,7 @@ package com.itheima.demo;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
