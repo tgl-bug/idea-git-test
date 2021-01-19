38a3bfa4326dccb3af68d12d2126887f32101286
tgl-bug <tgl812664988@gmail.com>
2021-01-19 13:46:02 +0800

    Revert "第一次使用idea提交代码"
    This reverts commit 6fa7dfb2

--- itheima-git-test01/src/com/itheima/demo/Demo01.java
+++ itheima-git-test01/src/com/itheima/demo/Demo01.java
@@ -3,10 +3,5 @@ package com.itheima.demo;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
     }
 }
